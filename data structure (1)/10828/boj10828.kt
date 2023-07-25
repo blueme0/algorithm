d08b63c309bbcd5d3c111dc7878488d93ee5e4a5
blueme0 <ksytpdus0304@gmail.com>
2023-07-25 21:53:14 +0900

    add: 10828 kotlin solution

---
+++ data structure (1)/10828/boj10828.kt
@@ -0,0 +1,24 @@
+fun main() = with(System.`in`.bufferedReader()) {
+    val N = readLine().toInt()
+    val numbers = mutableListOf<Int>()
+
+    repeat(N) {
+        val str = readLine().split(" ")
+        when (str[0]) {
+            "push" -> numbers.add(str[1].toInt())
+            "pop" -> {
+                if (numbers.isEmpty()) println(-1)
+                else println(numbers.removeLast())
+            }
+            "size" -> println(numbers.size)
+            "empty" -> {
+                if (numbers.isEmpty()) println(1)
+                else println(0)
+            }
+            "top" -> {
+                if (numbers.isEmpty()) println(-1)
+                else println(numbers.last())
+            }
+        }
+    }
+}

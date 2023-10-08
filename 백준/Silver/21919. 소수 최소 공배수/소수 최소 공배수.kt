4c05a4d84c2aeaa68f77d1bb0621794ded2401a6
seyeon <81468180+blueme0@users.noreply.github.com>
2023-10-09 03:17:33 +0900

    [Silver III] Title: 소수 최소 공배수, Time: 288 ms, Memory: 22892 KB -BaekjoonHub

---
+++ 백준/Silver/21919. 소수 최소 공배수/소수 최소 공배수.kt
@@ -0,0 +1,28 @@
+fun main() {
+    val br = System.`in`.bufferedReader()
+    val N = br.readLine().toInt()
+    val arr = br.readLine().split(" ").map{ it.toInt() }
+    var mul: Long = 1
+
+    val max = arr.max()
+    val isPrime = Array<Boolean>(max + 1) { true }
+    // 0, 1 are not Prime
+    isPrime[0] = false
+    isPrime[1] = false
+
+    for (i in 2..Math.sqrt(max.toDouble()).toInt() + 1) {
+        if (isPrime[i]) {
+            for (j in i * i..max step +i) {
+                isPrime[j] = false
+            }
+        }
+    }
+
+    for (num in arr) {
+        if (isPrime[num] && (mul % num.toLong()).toInt() != 0) mul *= num.toLong()
+    }
+
+    if (mul == 1.toLong()) mul = -1
+    println(mul)
+    
+}

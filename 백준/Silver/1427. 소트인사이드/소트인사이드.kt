4f72dfa721690156ff19f55d738a3ae90d74df49
seyeon <81468180+blueme0@users.noreply.github.com>
2024-03-27 15:11:15 +0900

    [Silver V] Title: 소트인사이드, Time: 120 ms, Memory: 18596 KB -BaekjoonHub

---
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.kt
@@ -0,0 +1,10 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val str = br.readLine().toCharArray()
+    str.sortDescending()
+
+    print(str)
+}

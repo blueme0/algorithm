523a48ee35845596c18e52e0da07c3df7c4898b9
seyeon <81468180+blueme0@users.noreply.github.com>
2024-07-10 20:10:40 +0900

    [Gold II] Title: 문제집, Time: 428 ms, Memory: 51616 KB -BaekjoonHub

---
+++ 백준/Gold/1766. 문제집/문제집.kt
@@ -0,0 +1,42 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.util.*
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    var st = StringTokenizer(br.readLine())
+    val N = st.nextToken().toInt()
+    val M = st.nextToken().toInt()
+
+    val arr = Array<Int>(N + 1) { 0 }
+    val graph = Array(N + 1) { mutableListOf<Int>() }
+
+    repeat(M) {
+        st = StringTokenizer(br.readLine())
+        val start = st.nextToken().toInt()
+        val end = st.nextToken().toInt()
+        arr[end]++
+        graph[start].add(end)
+    }
+
+    val zeros = PriorityQueue<Int>()
+    for (i in 1..N) {
+        if (arr[i] == 0) zeros.offer(i)
+    }
+
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+    while (zeros.isNotEmpty()) {
+        val cur = zeros.poll()
+        bw.write("$cur ")
+
+        for (i in graph[cur]) {
+            arr[i]--
+            if (arr[i] == 0) zeros.offer(i)
+        }
+    }
+
+    bw.flush()
+    bw.close()
+}

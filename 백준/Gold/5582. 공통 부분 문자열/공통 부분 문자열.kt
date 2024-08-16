ea484130ca404c1b4eb12c7f1b7a40e2f0ae85cb
seyeon <81468180+blueme0@users.noreply.github.com>
2024-08-16 22:30:19 +0900

    [Gold V] Title: 공통 부분 문자열, Time: 240 ms, Memory: 76880 KB -BaekjoonHub

--- 백준/Gold/5582. 공통 부분 문자열/공통 부분 문자열.kt
+++ 백준/Gold/5582. 공통 부분 문자열/공통 부분 문자열.kt
@@ -5,14 +5,16 @@ fun main() {
     val br = BufferedReader(InputStreamReader(System.`in`))
     val first = br.readLine()
     val second = br.readLine()
-    val dp = Array(first.length + 1) { IntArray(second.length + 1) { 0 } }
+    val flen = first.length
+    val slen = second.length
+    val dp = Array(flen + 1) { IntArray(slen + 1) { 0 } }
     var max = 0
 
-    for (i in 1..first.length) {
-        for (j in 1..second.length) {
+    for (i in 1..flen) {
+        for (j in 1..slen) {
             if (first[i - 1] == second[j - 1]) {
                 dp[i][j] = dp[i - 1][j - 1] + 1
-                max = Math.max(max, dp[i][j])
+                max = maxOf(max, dp[i][j])
             }
         }
     }
